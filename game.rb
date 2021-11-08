4ca0aa72870dbd1438b9a2d93a3d961bb420ffcf
habibcodes <habib.codes@gmail.com>
2021-11-08 18:53:52 -0500

    code cleanup and minor changes

--- game.rb
+++ game.rb
@@ -2,81 +2,49 @@
 # display lives
 class Game
 
+  # set up p1 and p2, set current player
   def initialize
     @player1 = Player.new("Player 1")
     @player2 = Player.new("Player 2")
     @current_player = @player1
   end
 
+  # player life check
   def game_over?
     !(@player1.game_over || @player2.game_over)
   end
 
   def start    
-    puts "Welcome to my game!"
-
+    puts "------Welcome to my game!------"
+    # ask questions until player life check met
     while game_over? do 
       question = Question.new
       current_question = question.ask_question
       
       puts "#{@current_player.name}: #{current_question}"
       
+      # take user input here
       puts ">"
       user_answer = gets.chomp
-
+      # if answer correct/if answer wrong, deduct life
       if user_answer.to_i == question.correct_answer
         puts "Yes! You are correct."
       else 
         puts "Seriously? No!"
         @current_player.deduct_life
       end
-
+      # show player score
       puts @current_player.get_score
-      
+        # switch current player
         if @current_player == @player1
           @current_player = @player2
         else
           @current_player = @player1
         end
       end
+      # if someone loses all life, print current winner
       puts "#{@current_player.name} wins with a score of #{@current_player.lives}/3"
-      puts "GAME OVER"
+      puts "------GAME OVER------"
     
   end
-
-
-
-    
-
-
-
-
-
-
-  #  # displays p1 and p2 scores
-  #  def show_score
-  #   "#{get_score} vs #{get_score}"
-  # end
-
-
-  # def new_question
-  #   # new_question = Question.new
-  #   new_question.ask_question(name)
-
-  #   print '> '
-
-
-  #   @userchoice = $stdin.gets.chomp
-
-
-  #   if new_question.check_answer?(@userchoice.to_i)
-  #     puts 'Yes! You are correct.'
-  #   else
-  #     puts 'Seriously? No!'
-  #     deduct_life
-  #   end
-  # end
-
-
-
 end
